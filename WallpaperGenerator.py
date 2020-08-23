0f88d83cc7a6ac2ad2bbb0cac926b17a4f70eec6
Mikhail <mihellad@gmail.com>
2020-08-23 22:56:21 +1000

    add new images

--- WallpaperGenerator.py
+++ WallpaperGenerator.py
@@ -1,6 +1,6 @@
 import requests
 
-for i in range(41, 60):
+for i in range(60, 101):
     img = requests.get("https://picsum.photos/1920/1080")
     img_file = open('D:\Code\Web\Проекты\WallpaperGenerator\images\img' + str(i) + '.jpg', 'wb')
     img_file.write(img.content)
